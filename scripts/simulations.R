a426938add66758da07e24216dea6f093f023398
Cody Markelz <rjmarkelz@ucdavis.edu>
2015-01-20 16:38:53 -0800

    add QTL container

--- scripts/simulations.R
+++ scripts/simulations.R
@@ -2,6 +2,8 @@
 # quick code to play with some QTL assumptions for a simulated brassica rapa population.
 library(qtl)
 
+#some of this involves random draws
+set.seed(123567)
 # simulate brassica cross
 # this is for version 1.2 NOT 1.5 of the map
 n.ind <- 124
@@ -21,7 +23,8 @@ head(genotypes)
 geno.names <- dimnames(genotypes)[[2]]
 
 # Demo values using brassica marker simulated data
-# sample significant markers for our 5 simulated phenotypes
+# sample significant markers for our 4 simulated phenotypes
+# or define them as you wish
 m1 <- sample(geno.names, 3, replace = FALSE)
 m2 <- sample(geno.names, 2, replace = FALSE)
 m3 <- sample(geno.names, 2, replace = FALSE)
@@ -31,14 +34,14 @@ m4 <- sample(geno.names, 1, replace = FALSE)
 g11 <- genotypes[,m1[1]]; g12 <- genotypes[,m1[2]]; g13 <- genotypes[,m1[3]]
 g21 <- genotypes[,m2[1]]; g22 <- genotypes[,m2[2]]
 g31 <- genotypes[,m3[1]]; g32 <- genotypes[,m3[2]]
-g41 <- genotypes[,m4[1]]; g42 <- genotypes[,m4[2]]
+g41 <- genotypes[,m4[1]]; 
 
 # generate correlated phenotypes, this is where you can generate any number of phenotypes that are or 
 # are not correlated with one another
 y1 <- runif(3,0.5,1)[g11] + runif(3,0.5,1)[g12] + rnorm(n.ind)
 y2 <- runif(3,0.5,1)[g21] + runif(3,0.5,1)[g22] + rnorm(n.ind)
 y3 <- runif(1,0.5,1) * y1 + runif(1,0.5,1) * y2 + runif(3,0.5,1)[g31] + runif(3,0.5,1)[g32] + rnorm(n.ind)
-y4 <- runif(1,0.5,1) * y3 + runif(3,0.5,1)[g41] + runif(3,0.5,1)[g42] + rnorm(n.ind)
+y4 <- runif(1,0.5,1) * y3 + runif(3,0.5,1)[g41] + rnorm(n.ind)
 
 # take a quick look
 cor(y1,y2)
@@ -46,3 +49,30 @@ cor(y2,y3)
 cor(y3,y4)
 cor(y2,y4)
 cor(y1,y3)
+
+# start with empty list
+allqtls <- list()
+m1.pos <- find.markerpos(br_test_cross, m1)
+m2.pos <- find.markerpos(br_test_cross, m2)
+m3.pos <- find.markerpos(br_test_cross, m3)
+m4.pos <- find.markerpos(br_test_cross, m4)
+?makeqtl
+
+# you can choose to do more draws etc.
+br_test_cross <- sim.geno(br_test_cross, n.draws = 8, step = 2, err = 0.001)
+
+# you can put all of these into a list, or do one at a time
+# if you do one at a time, then the regular RQTL functions will work
+# leave it up to you to decide
+allqtls[[1]] <- makeqtl(br_test_cross, chr = m1.pos[,"chr"], pos = m1.pos[,"pos"])
+allqtls[[2]] <- makeqtl(br_test_cross, chr = m2.pos[,"chr"], pos = m2.pos[,"pos"])
+allqtls[[3]] <- makeqtl(br_test_cross, chr = m3.pos[,"chr"], pos = m3.pos[,"pos"])
+allqtls[[4]] <- makeqtl(br_test_cross, chr = m4.pos[,"chr"], pos = m4.pos[,"pos"])
+names(allqtls) <- c("trait1","trait2","trait3","trait4")
+
+# or if you want to fit multi qtl models directly
+multiqtl <- makeqtl(br_test_cross, c(chr = m1.pos[,"chr"], m2.pos[,"chr"]),
+                    pos = c(m1.pos[,"pos"], m2.pos[,"pos"]))
+summary(multiqtl)
+plot(multiqtl)
+
